5312945cf603e7709fe7f7c88b6923efa0370078
Thais Attico <contato.thaisattico@gmail.com>
2026-08-31 14:11:10 -0300

    Enhance SQL query with explanatory comments
    Added comments to clarify the query's purpose and criteria for punctuality.

--- Queries/pontualidade_por_empresa.sql
+++ Queries/pontualidade_por_empresa.sql
@@ -1,3 +1,8 @@
+--Top 10 empresas mais pontuais, tanto em partida quanto em chegada
+--Um voo só é considerado pontual se ambos (partida e chegada) forem pontuais (existem voos que saem antecipados,
+--mas chegam atrasados, e vice-versa)
+--HAVING COUNT(*) >= 500 filtra empresas com poucos voos no período, evitando distorção na taxa por amostras pequenas
+
 SELECT "Sigla ICAO Empresa Aérea", COUNT(*) AS voos_gerais,
 ROUND(SUM(CASE WHEN "Situação Partida" = 'Pontual' AND "Situação Chegada"= 'Pontual'
 Then 1
