b122eecc3c1e579501dfa7409682c5331a26eb77
Thais Attico <contato.thaisattico@gmail.com>
2026-08-31 13:54:40 -0300

    Enhance SQL query with comments for clarity
    Added comments to clarify the purpose of the query and its structure.

--- Queries/cancelamento_por_aeroporto.sql
+++ Queries/cancelamento_por_aeroporto.sql
@@ -1,9 +1,12 @@
+--Top 10 aeroportos com mais cancelamentos
+--Conta o aeroporto tanto quando é origem quanto quando é destino do voo
+
 SELECT aeroporto,
        SUM(CASE WHEN "Situação Voo" = 'CANCELADO' THEN 1 ELSE 0 END) AS cancelamento_por_aeroporto
-FROM (
+FROM ( --a subquery permite agrupar por aeroporto de forma unificada
     SELECT "Descrição Aeroporto Origem" AS aeroporto, "Situação Voo"
     FROM vra_2024
-    UNION ALL
+    UNION ALL --empilha as duas colunas
     SELECT "Descrição Aeroporto Destino" AS aeroporto, "Situação Voo"
     FROM vra_2024
 ) AS todos_aeroportos
